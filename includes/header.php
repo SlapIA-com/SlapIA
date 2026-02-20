50f4ebff0bc3da69b0ea396de7d177c2ebb863f8
ThomasLap13 <thomas25.lapierre@outlook.com>
2026-02-20 18:37:50 +0100

    V14.13 : Modification formation

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@ include_once 'lang.php';
 
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
+<html lang="<?php echo $lang ?? 'fr'; ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
